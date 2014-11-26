a1e18837b6f7caca5e2e8f49d674aaab864c9da4
Quramy <yosuke.kurami@gmail.com>
2014-11-26 18:27:49 +0000

    UPDATE DOCS.

--- modules.js
+++ modules.js
@@ -89,6 +89,40 @@ angular.module('dgeniNgdocExample')
     });
   }]);
 
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name awesome
+ * @restrict E
+ * @module dgeniNgdocExample
+ *
+ * @description
+ *
+ * It is an awesome directive.
+ *
+ * @example
+ 	 <example module="awsomeExample" deps="" animate="false">
+      <file name="index.html">
+        <div ng-controller="ExampleController as example">
+          <awesome></awesome>
+        </div>
+      </file>
+		 	<file name="script.js">
+        angular.module('awsomeExample', ['dgeniNgdocExample']).controller('ExampleController', function(){
+          var example = this;
+        });
+			</file>
+	 </example>
+ *
+ **/
+angular.module('dgeniNgdocExample').directive('awesome', function(){
+	return{
+		restrict: 'E',
+		template: '<div>This is an Awesome!</div>'
+	};
+});
+
 'use strict'
 
 /**
@@ -171,40 +205,6 @@ angular.module('dgeniNgdocExample').provider('message', function(){
 				}
 			};
 		}
-	};
-});
-
-'use strict';
-
-/**
- * @ngdoc directive
- * @name awesome
- * @restrict E
- * @module dgeniNgdocExample
- *
- * @description
- *
- * It is an awesome directive.
- *
- * @example
- 	 <example module="awsomeExample" deps="" animate="false">
-      <file name="index.html">
-        <div ng-controller="ExampleController as example">
-          <awesome></awesome>
-        </div>
-      </file>
-		 	<file name="script.js">
-        angular.module('awsomeExample', ['dgeniNgdocExample']).controller('ExampleController', function(){
-          var example = this;
-        });
-			</file>
-	 </example>
- *
- **/
-angular.module('dgeniNgdocExample').directive('awesome', function(){
-	return{
-		restrict: 'E',
-		template: '<div>This is an Awesome!</div>'
 	};
 });
 
