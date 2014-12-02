cba17362ddcfe25fa1175e2d1035998efd25c03f
Quramy <yosuke.kurami@gmail.com>
2014-12-02 11:04:12 +0000

    UPDATE DOCS.

--- modules.js
+++ modules.js
@@ -1,9 +1,11 @@
 'use strict';
 
 /**
+ *
  * @ngdoc module
  * @name dgeniNgdocExample
  * @module dgeniNgdocExample
+ * @packageName dgeni-ngdoc-example
  * @description
  *
  * This is a ngdoc example application module.
@@ -119,7 +121,7 @@ angular.module('dgeniNgdocExample')
 angular.module('dgeniNgdocExample').directive('awesome', function(){
 	return{
 		restrict: 'E',
-		template: '<div>This is an Awesome!</div>'
+		template: '<div class="awesome">This is an Awesome!</div>'
 	};
 });
 
@@ -140,41 +142,41 @@ angular.module('dgeniNgdocExample').directive('awesome', function(){
  *
  **/
 angular.module('dgeniNgdocExample').provider('message', function(){
-	var repos = {};
-	return {
+  var repos = {};
+  return {
 
-		/**
-		 * @ngdoc method
-		 * @name messageProvider#put
-		 * @description
-		 * Put a message to repository. See also `{@link message#get message#get}`.
-		 *
-		 *
-		 * @param {string} id The message's ID.
-		 * @param {string} message Content of this message.
-		 *
-		 **/
-		put: function (id, messagge){
-			repos[id] = messagge;
-		},
+    /**
+     * @ngdoc method
+     * @name messageProvider#put
+     * @description
+     * Put a message to repository. See also `{@link message#get message#get}`.
+     *
+     *
+     * @param {string} id The message's ID.
+     * @param {string} message Content of this message.
+     *
+     **/
+    put: function (id, messagge){
+      repos[id] = messagge;
+    },
 
-		/**
-		 * @ngdoc service
-		 * @name message
-		 * @module dgeniNgdocExample
-		 * 
-		 * @description
-		 *
-		 * This service provides messages to your app.
-		 *
-		 * @example
-	 <example module="messageExample" deps="" animate="false">
+    /**
+     * @ngdoc service
+     * @name message
+     * @module dgeniNgdocExample
+     * 
+     * @description
+     *
+     * This service provides messages to your app.
+     *
+     * @example
+   <example module="messageExample" deps="" animate="false">
       <file name="index.html">
         <div ng-controller="ExampleController as example">
-          messages: <span>{{example.msg}}</span>
+          messages: {{example.msg}}
         </div>
       </file>
-		 	<file name="script.js">
+       <file name="script.js">
         angular.module('messageExample', ['dgeniNgdocExample'])
         .config(function (messageProvider) {
           messageProvider.put('msg01', 'Hello, world!');
@@ -183,29 +185,29 @@ angular.module('dgeniNgdocExample').provider('message', function(){
           var example = this;
           example.msg = message.get('msg01');
         });
-			</file>
-	 </example>
-		 **/
-		$get: function () {
-			return {
+      </file>
+   </example>
+     **/
+    $get: function () {
+      return {
 
-				/**
-				 * @ngdoc method
-				 * @name message#get
-				 *
-				 * @description
-				 * Get a messages by ID.
-				 *
-				 * @param {string} id A message ID.
-				 * @return {string} message
-				 *
-				 **/
-				get: function(id){
-					return repos[id];
-				}
-			};
-		}
-	};
+        /**
+         * @ngdoc method
+         * @name message#get
+         *
+         * @description
+         * Get a messages by ID.
+         *
+         * @param {string} id A message ID.
+         * @return {string} message
+         *
+         **/
+        get: function(id){
+          return repos[id];
+        }
+      };
+    }
+  };
 });
 
 'use strict';
